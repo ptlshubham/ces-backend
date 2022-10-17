54de83f092acd0149a2cdbdfdf39b2c6fc9fc4ab
Shubham Patel <ptlshubham@gmail.com>
2022-10-17 15:42:54 +0530

    Ces Home

--- src/routes/user.js
+++ src/routes/user.js
@@ -8,7 +8,6 @@ var midway = require('./midway');
 const jwt = require('jsonwebtoken');
 const nodemailer = require('nodemailer');
 
-
 function generateUUID() {
     var d = new Date().getTime();
     var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx'.replace(/[xy]/g, function (c) {
@@ -19,5 +18,21 @@ function generateUUID() {
 
     return uuid;
 }
+
+router.post("/GetHomePageImages", (req, res, next) => {
+    db.executeSql("select * from web_home_image where name='" + req.body.name + "' && isactive=true", function (data, err) {
+        if (err) {
+            console.log("Error in store.js", err);
+        } else {
+            return res.json(data);
+        }
+    });
+})
+
+
+
+
+
+
 
 module.exports = router;
